01f380febec950adb764a6fac479e837062ccfba
Alex Harms <onealexharms@gmail.com>
2020-08-27 20:56:02 -0400

    Extract position

--- src/opencircuit/core.clj
+++ src/opencircuit/core.clj
@@ -3,6 +3,10 @@
     [dali.io]
     [dali.layout.distribute]))
 
+(defn position [thing x y]
+  [:g {:transform [:translate [x y]]}
+   thing])
+
 (defmacro define [name value]
   `(def ^:private ~name ~value))
 
