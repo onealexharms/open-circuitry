2249c066d76a967ef52f64273d4d0c9de43f436b
Alex Harms <onealexharms@gmail.com>
2020-09-06 17:49:01 -0400

    Extract attributes function

--- src/open_circuitry/svg.clj
+++ src/open_circuitry/svg.clj
@@ -1,8 +1,11 @@
 (ns open-circuitry.svg)
 
+(defn attributes [node]
+  (second node))
+
 (defn rendering
   [board]
-  (let [attributes (second board)]
+  (let [attributes (attributes board)]
     (if (:width attributes)
       [nil {:width (:width attributes)}]
       (throw (Exception. "A board needs a :width attribute")))))
