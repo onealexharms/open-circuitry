b3c75c42848ec6a7cd5d29b345ef06533cd59f91
Alex Harms <onealexharms@gmail.com>
2020-09-13 15:47:22 -0400

    Group tests

--- test/open_circuitry/svg_test.clj
+++ test/open_circuitry/svg_test.clj
@@ -1,7 +1,7 @@
 (ns open-circuitry.svg-test
  (:require
   [clojure.java.io :as io]
-  [clojure.test :refer [deftest is]]
+  [clojure.test :refer [deftest testing is]]
   [dali.io]
   [net.cgrand.enlive-html :as enlive]
   [open-circuitry.svg :refer [dali-rendering]]))
@@ -31,33 +31,30 @@
 (defn svg-attributes [board selector]
   (:attrs (svg-element board selector)))
 
-(deftest has-a-cutout-toolpath
-  (let [board [:open-circuitry/board {:width 10 :height 10}]
-        cutout-toolpath (svg-element board [:g#cutout-toolpath])]
-   (is cutout-toolpath)))
-
-(deftest cutout-has-zero-origin 
-  (let [board [:open-circuitry/board {:width 10 :height 20}]
-        {:keys [x y]} (svg-attributes board [:g#cutout-toolpath :rect])]
-    (is (= "0" x))
-    (is (= "0" y))))
-
-(deftest cutout-size-matches-board-size
-  (let [board [:open-circuitry/board {:width 10 :height 20}]
-        {:keys [width height]} (svg-attributes board [:g#cutout-toolpath :rect])]
-    (is (= "10" width))
-    (is (= "20" height))))
-
-(deftest cutout-has-white-fill
-  (let [board [:open-circuitry/board {:width 10 :height 20}]
-        {:keys [fill]} (svg-attributes board [:g#cutout-toolpath :rect])]
-    (is (= "white" fill))))
-
-(deftest cutout-has-red-stroke
-  (let [board [:open-circuitry/board {:width 10 :height 20}]
-        {:keys [stroke]} (svg-attributes board [:g#cutout-toolpath :rect])]
-    (is (= "red" stroke))))
-
+(deftest cutout
+  (testing "has a cutout toolpath"
+    (let [board [:open-circuitry/board {:width 10 :height 10}]
+          cutout-toolpath (svg-element board [:g#cutout-toolpath])]
+     (is cutout-toolpath)))
+  (testing "has a zero origin"
+    (let [board [:open-circuitry/board {:width 10 :height 20}]
+          {:keys [x y]} (svg-attributes board [:g#cutout-toolpath :rect])]
+      (is (= "0" x))
+      (is (= "0" y))))
+  (testing "size matches board size"
+    (let [board [:open-circuitry/board {:width 10 :height 20}]
+          {:keys [width height]} (svg-attributes board [:g#cutout-toolpath :rect])]
+      (is (= "10" width))
+      (is (= "20" height))))
+  (testing "has white fill"
+    (let [board [:open-circuitry/board {:width 10 :height 20}]
+          {:keys [fill]} (svg-attributes board [:g#cutout-toolpath :rect])]
+      (is (= "white" fill))))
+  (testing "has red stroke"
+    (let [board [:open-circuitry/board {:width 10 :height 20}]
+          {:keys [stroke]} (svg-attributes board [:g#cutout-toolpath :rect])]
+      (is (= "red" stroke)))))
+  
 (deftest a-50x100-board-rendering-is-50mm-wide-100mm-high
   (let [board [:open-circuitry/board {:width 50 :height 100}]
         {:keys [width height]} (svg-attributes board [:svg])]
