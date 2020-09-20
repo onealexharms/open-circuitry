25800edfcebb7329cbc7a0282f894e600f366999
Alex Harms <onealexharms@gmail.com>
2020-09-20 16:13:54 -0400

    Refactor drilled-juncture test

--- test/open_circuitry/junctures_test.clj
+++ test/open_circuitry/junctures_test.clj
@@ -4,13 +4,14 @@
     [net.cgrand.enlive-html :as enlive]
     [open-circuitry.test-helpers :as test]))
 
-(deftest a-single-juncture
+(deftest drilled-juncture
   (let [board [:open-circuitry/board {:width 10, :height 10}
-               [:juncture {:drill 2.3}]]]
-    (testing "describes a toolpath"
-      (is (test/svg-element board [[:g (enlive/attr= :id "drill-2.3mm")]])))
-    (testing "drill hole is a circle"
-      (is (test/svg-element board [(enlive/attr= :id "drill-2.3mm") :> :circle])))
-    (testing "drill hole circle has a radius of 0.02"
-      (let [radius (:r (test/svg-attributes board [(enlive/attr= :id "drill-2.3mm") :> :circle]))]
+               [:juncture {:drill 2.3}]]
+        drill-toolpath (enlive/attr= :id "drill-2.3mm")] 
+    (testing "has a drill toolpath"
+      (is (test/svg-element board [[:g drill-toolpath]])))
+    (testing "is a circle"
+      (is (test/svg-element board [drill-toolpath :> :circle])))
+    (testing "drill-hole circle has a radius of 0.02"
+      (let [radius (:r (test/svg-attributes board [drill-toolpath :> :circle]))]
         (is (= "0.02" radius))))))
