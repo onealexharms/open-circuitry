74c4cffdc6e7eadaf08c062326957751b41e072d
Alex Harms <onealexharms@gmail.com>
2020-09-28 18:37:13 -0400

    Refactor tests.

--- test/open_circuitry/svg_test.clj
+++ test/open_circuitry/svg_test.clj
@@ -4,33 +4,36 @@
   [open-circuitry.test-helpers :as test]
   [open-circuitry.svg :refer [dali-rendering]]))
 
+(defmacro exists [thing]
+  `(is ~thing))
+
 (deftest a-rendered-board
   (testing "that's 50 x 100"
     (let [board [:open-circuitry/board {:width 50 :height 100}]
           {:keys [width height viewBox]} (test/attributes-by-selector [:svg] board)]
-      (testing "is 50mm wide"
+      (testing "has width of 50mm"
         (is (= "50mm" width)))
-      (testing "is 100mm high"
+      (testing "has height of 100mm"
         (is (= "100mm" height)))
-      (testing "is in millimeters"
-        (is (= "0 0 50 100" viewBox)))
-      (testing "has dimensions"
-        (testing "a width"
-          (is (thrown-with-msg? Exception
-                                #"A board needs attribute: :width"
-                                (dali-rendering [:open-circuitry/board
-                                                 {:height 7}]))))
-        (testing "a height"
-          (is (thrown-with-msg? Exception
-                                #"A board needs attribute: :height"
-                                (dali-rendering [:open-circuitry/board
-                                                 {:width 9}]))))))))
+      (testing "uses millimeters"
+        (is (= "0 0 50 100" viewBox)))))
+  (testing "is only possible if"
+    (testing "it has a width"
+      (is (thrown-with-msg? Exception
+                            #"A board needs attribute: :width"
+                            (dali-rendering [:open-circuitry/board
+                                             {:height 7}]))))
+    (testing "it has a height"
+      (is (thrown-with-msg? Exception
+                            #"A board needs attribute: :height"
+                            (dali-rendering [:open-circuitry/board
+                                             {:width 9}]))))))
 
-(deftest cutout
-  (testing "has a cutout toolpath"
+(deftest the-cutout-toolpath
+  (testing "exists"
     (let [board [:open-circuitry/board {:width 10 :height 10}]
           cutout-toolpath (test/element-by-selector [:g#cutout-toolpath] board)]
-     (is cutout-toolpath)))
+     (exists cutout-toolpath)))
   (testing "has a zero origin"
     (let [board [:open-circuitry/board {:width 10 :height 20}]
           {:keys [x y]} (test/attributes-by-selector [:g#cutout-toolpath :rect] board)]
