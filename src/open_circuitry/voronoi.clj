6cb2081260bb55aa7199b4da37e992e29922824b
Alex Harms <onealexharms@gmail.com>
2020-10-24 16:54:43 -0400

    voronoi edges list their generator points

--- src/open_circuitry/voronoi.clj
+++ src/open_circuitry/voronoi.clj
@@ -2,6 +2,9 @@
   (:import
    (org.kynosarges.tektosyne.geometry PointD RectD Voronoi)))
 
+(defn- ->point [^PointD p]
+  [(.x p) (.y p)])
+
 (defn voronoi
   [points [[left top] [width height]]]
   (let [PointDs      (into-array PointD (map (fn [[x y]]
@@ -11,13 +14,13 @@
         voronoi-object   (Voronoi/findAll PointDs bounds)
         voronoi-vertices (.voronoiVertices voronoi-object)
         voronoi-edges    (.voronoiEdges voronoi-object)
-        vertices         (vec (map (fn [^PointD vertex]
-                                     [(.x vertex) (.y vertex)])
-                                   voronoi-vertices))
+        generator-sites  (.generatorSites voronoi-object)
+        vertices         (vec (map ->point voronoi-vertices))
         edges            (vec (map (fn [edge]
                                      {:start            (get vertices (.vertex1 edge))
                                       :end              (get vertices (.vertex2 edge))
-                                      :generator-points "string"})
+                                      :generator-points [(->point (get generator-sites (.site1 edge)))
+                                                         (->point (get generator-sites (.site2 edge)))]})
                                    voronoi-edges))]
     {:vertices vertices
      :edges    edges}))
--- test/open_circuitry/voronoi_test.clj
+++ test/open_circuitry/voronoi_test.clj
@@ -6,7 +6,7 @@
 
 (deftest voronoi-diagrams
   (testing "edges"
-    (testing "have two generator points"
-      (let [diagram (voronoi [[2 2] [5 5]] [[0 0] [10 10]])
-            {:keys [edges]} diagram]
-        (exists (every? :generator-points edges))))))
+    (testing "list their generator points"
+      (let [diagram          (voronoi [[2.5 2] [5 5.0]] [[0 0] [10 10]])
+            generator-points (:generator-points (first (:edges diagram)))]
+        (is (= (sort generator-points) [[2.5 2.0] [5.0 5.0]]))))))
