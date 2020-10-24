3e0cd630f2b27293552401bbadb8ac8a9f889f21
Alex Harms <onealexharms@gmail.com>
2020-10-24 17:21:00 -0400

    Rename to tektosyne-voronoi etc

--- src/open_circuitry/voronoi.clj
+++ src/open_circuitry/voronoi.clj
@@ -10,18 +10,18 @@
 
 (defn voronoi
   [points [[left top] [width height]]]
-  (let [pointDs          (into-array PointD (map ->PointD points))
-        bounds           (RectD. (PointD. left top) (PointD. (+ left width) (+ top height)))
-        voronoi-object   (Voronoi/findAll pointDs bounds)
-        voronoi-vertices (.voronoiVertices voronoi-object)
-        voronoi-edges    (.voronoiEdges voronoi-object)
-        generator-points (vec (map ->point (.generatorSites voronoi-object)))
-        vertices         (vec (map ->point voronoi-vertices))
-        ->edge           (fn [edge]
-                           {:start            (get vertices (.vertex1 edge))
-                            :end              (get vertices (.vertex2 edge))
-                            :generator-points [(get generator-points (.site1 edge))
-                                               (get generator-points (.site2 edge))]})
-        edges            (vec (map ->edge voronoi-edges))]
+  (let [pointDs             (into-array PointD (map ->PointD points))
+        tektosyne-bounds    (RectD. (PointD. left top) (PointD. (+ left width) (+ top height)))
+        tektosyne-voronoi   (Voronoi/findAll pointDs tektosyne-bounds)
+        tektosyne-vertices  (.voronoiVertices tektosyne-voronoi)
+        tektosyne-edges     (.voronoiEdges tektosyne-voronoi)
+        generator-points    (vec (map ->point (.generatorSites tektosyne-voronoi)))
+        vertices            (vec (map ->point tektosyne-vertices))
+        ->edge              (fn [edge]
+                              {:start            (get vertices (.vertex1 edge))
+                               :end              (get vertices (.vertex2 edge))
+                               :generator-points [(get generator-points (.site1 edge))
+                                                  (get generator-points (.site2 edge))]})
+        edges               (vec (map ->edge tektosyne-edges))]
     {:vertices vertices
      :edges    edges}))
