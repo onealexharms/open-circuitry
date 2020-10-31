c8d6607d4af685c7bdeaaaf5a4a981cde71e107b
Alex Harms <onealexharms@gmail.com>
2020-10-31 18:17:34 -0400

    Express tektosynes's weird indexes thing.

--- src/open_circuitry/voronoi.clj
+++ src/open_circuitry/voronoi.clj
@@ -15,20 +15,25 @@
         end          (->point (.end clipped-line))]
     [start end]))
 
+
 (defn- edges [tektosyne-voronoi tektosyne-bounds] 
   (let [generator-points   (vec (map ->point (.generatorSites tektosyne-voronoi)))]
     (vec (for [tektosyne-edge (.voronoiEdges tektosyne-voronoi)]
            (let [tektosyne-vertices (.voronoiVertices tektosyne-voronoi)
-                 [start end] (bounded-edge tektosyne-bounds
-                                           (get tektosyne-vertices (.vertex1 tektosyne-edge))
-                                           (get tektosyne-vertices (.vertex2 tektosyne-edge)))]
+                 index-of-vertex1   (.vertex1 tektosyne-edge)
+                 index-of-vertex2   (.vertex2 tektosyne-edge)
+                 [start end]        (bounded-edge 
+                                      tektosyne-bounds
+                                      (get tektosyne-vertices index-of-vertex1)
+                                      (get tektosyne-vertices index-of-vertex2))
+                 index-of-site1     (.site1 tektosyne-edge)
+                 index-of-site2     (.site2 tektosyne-edge)]
              {:start            start
               :end              end
-              :generator-points [(get generator-points (.site1 tektosyne-edge))
-                                 (get generator-points (.site2 tektosyne-edge))]})))))
+              :generator-points [(get generator-points index-of-site1)
+                                 (get generator-points index-of-site2)]})))))
 
-(defn voronoi
-  [points [[left top] [width height]]]
+(defn voronoi [points [[left top] [width height]]]
   (let [pointDs             (into-array PointD (map ->PointD points))
         tektosyne-bounds    (RectD. (PointD. left top) (PointD. (+ left width) (+ top height)))
         tektosyne-voronoi   (Voronoi/findAll pointDs tektosyne-bounds)
