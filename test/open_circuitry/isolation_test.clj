9b3e2b35760f8aef8cf11f01ac34d0bf4e26d910
Alex Harms <onealexharms@gmail.com>
2020-10-31 16:47:21 -0400

    Move test

--- test/open_circuitry/isolation_test.clj
+++ test/open_circuitry/isolation_test.clj
@@ -1,10 +1,7 @@
 (ns open-circuitry.isolation-test
   (:require
    [clojure.test :refer [deftest testing is]]
-   [open-circuitry.test-helpers :refer :all])
-  (:import
-   (org.kynosarges.tektosyne.subdivision Subdivision)
-   (org.kynosarges.tektosyne.geometry LineD PointD)))
+   [open-circuitry.test-helpers :refer :all]))
 
 (deftest an-isolation-toolpath
   (testing "for a board with no junctures"
@@ -19,60 +16,3 @@
                    [:juncture {:at [5.0 5.0]}]]
             paths (elements-by-selector [:#isolation-toolpath :> :line] board)]
         (is (not (empty? paths)))))))
-
-(defn line->LineD [line]
-  (LineD. (read-string (:x1 (:attrs line)))
-          (read-string (:y1 (:attrs line)))
-          (read-string (:x2 (:attrs line)))
-          (read-string (:y2 (:attrs line)))))
-
-(defn cutout-LineDs [board]
-  (let [rectangle (element-by-selector [:#cutout-toolpath :> :rect] board)
-        {:keys [x y width height]} (:attrs rectangle)
-        left        (read-string x)
-        top         (read-string y)
-        right       (+ left (read-string width))
-        bottom      (+ top (read-string height))]
-    (into-array LineD [(LineD. left top right top)
-                       (LineD. right top right bottom)
-                       (LineD. right bottom left bottom)
-                       (LineD. left bottom left top)])))
-
-(defn isolation-LineDs [board]
-  (->> (elements-by-selector [:#isolation-toolpath :> :line] board)
-       (map line->LineD) 
-       (into-array LineD)))
-
-(defn isolated? [location1 location2 board]
-  (let [cutout          (Subdivision/fromLines (cutout-LineDs board) 0.001)
-        isolation-lines (Subdivision/fromLines (isolation-LineDs board) 0.001)
-        divided-plane   (.division (Subdivision/intersection cutout isolation-lines)) 
-        face            (fn [[x y]]
-                          (.findFace divided-plane (PointD. x y)))]
-    (not (identical? (face location1) (face location2)))))
-  
-(deftest junctures
-  (testing "that aren't connected"
-    (testing "are isolated"
-      (let [GND-coordinates [7 8]
-            VCC-coordinates [5 5]
-            abc-coordinates [1 2]
-            board           [:open-circuitry/board {:width 10 :height 20}
-                             [:juncture {:at GND-coordinates, :trace "GND"}]
-                             [:juncture {:at VCC-coordinates, :trace "VCC"}]
-                             [:juncture {:at abc-coordinates, :trace "abc"}]]]
-        (is (isolated? GND-coordinates VCC-coordinates board))
-        (is (isolated? GND-coordinates abc-coordinates board))
-        (is (isolated? abc-coordinates VCC-coordinates board)))))
-  (testing "that share a Voronoi edge and share a trace"
-    (testing "are not isolated"
-      (let [GND-coordinates [7 8]
-            VCC-start       [5 5]
-            VCC-end         [1 2]
-            board           [:open-circuitry/board {:width 10 :height 20}
-                             [:juncture {:at GND-coordinates, :trace "GND"}]
-                             [:juncture {:at VCC-start, :trace "VCC"}]
-                             [:juncture {:at VCC-end, :trace "VCC"}]]]
-        (is (isolated? GND-coordinates VCC-start board))
-        (is (isolated? GND-coordinates VCC-end board))
-        (is (not (isolated? VCC-start VCC-end board)))))))
--- test/open_circuitry/junctures_test.clj
+++ test/open_circuitry/junctures_test.clj
@@ -4,7 +4,10 @@
     [net.cgrand.enlive-html :as enlive]
     [open-circuitry.data-tree :as data]
     [open-circuitry.test-helpers :refer :all]
-    [open-circuitry.svg :refer [dali-rendering]]))
+    [open-circuitry.svg :refer [dali-rendering]])
+  (:import
+   (org.kynosarges.tektosyne.subdivision Subdivision)
+   (org.kynosarges.tektosyne.geometry LineD PointD)))
 
 (deftest a-drill-toolpath
   (testing "containing two 5mm drilled junctures"
@@ -21,7 +24,7 @@
       (testing "exists once and only once"
         (is (= 1 (count (toolpaths-with-id toolpath-id board)))))
       (testing "contains only junctures that are drilled"
-        (is (empty? (elements-by-selector 
+        (is (empty? (elements-by-selector
                       [toolpath-selector :> (location-selector 5 6)]
                       board))))
       (testing "does not contain junctures of a different diameter"
@@ -41,12 +44,69 @@
                  [:juncture {:at [4 3]}]
                  [:juncture {:at [3 7]}]]]
       (testing "does not exist"
-        (is (empty? (elements-by-selector [(enlive/attr|= :id "drill")] board))))))) 
+        (is (empty? (elements-by-selector [(enlive/attr|= :id "drill")] board)))))))
 
 (deftest a-juncture
-  (testing "requires" 
+  (testing "requires"
     (testing "a position"
       (is (thrown-with-msg? Exception
                             #"A juncture needs attribute: :at"
                             (dali-rendering [:open-circuitry/board {:width 10, :height 10}
                                              [:juncture {:drill 22.7}]]))))))
+
+(defn line->LineD [line]
+  (LineD. (read-string (:x1 (:attrs line)))
+          (read-string (:y1 (:attrs line)))
+          (read-string (:x2 (:attrs line)))
+          (read-string (:y2 (:attrs line)))))
+
+(defn cutout-LineDs [board]
+  (let [rectangle (element-by-selector [:#cutout-toolpath :> :rect] board)
+        {:keys [x y width height]} (:attrs rectangle)
+        left        (read-string x)
+        top         (read-string y)
+        right       (+ left (read-string width))
+        bottom      (+ top (read-string height))]
+    (into-array LineD [(LineD. left top right top)
+                       (LineD. right top right bottom)
+                       (LineD. right bottom left bottom)
+                       (LineD. left bottom left top)])))
+
+(defn isolation-LineDs [board]
+  (->> (elements-by-selector [:#isolation-toolpath :> :line] board)
+       (map line->LineD)
+       (into-array LineD)))
+
+(defn isolated? [location1 location2 board]
+  (let [cutout          (Subdivision/fromLines (cutout-LineDs board) 0.001)
+        isolation-lines (Subdivision/fromLines (isolation-LineDs board) 0.001)
+        divided-plane   (.division (Subdivision/intersection cutout isolation-lines))
+        face            (fn [[x y]]
+                          (.findFace divided-plane (PointD. x y)))]
+    (not (identical? (face location1) (face location2)))))
+
+(deftest junctures
+  (testing "that aren't connected"
+    (testing "are isolated"
+      (let [GND-coordinates [7 8]
+            VCC-coordinates [5 5]
+            abc-coordinates [1 2]
+            board           [:open-circuitry/board {:width 10 :height 20}
+                             [:juncture {:at GND-coordinates, :trace "GND"}]
+                             [:juncture {:at VCC-coordinates, :trace "VCC"}]
+                             [:juncture {:at abc-coordinates, :trace "abc"}]]]
+        (is (isolated? GND-coordinates VCC-coordinates board))
+        (is (isolated? GND-coordinates abc-coordinates board))
+        (is (isolated? abc-coordinates VCC-coordinates board)))))
+  (testing "that share a Voronoi edge and share a trace"
+    (testing "are not isolated"
+      (let [GND-coordinates [7 8]
+            VCC-start       [5 5]
+            VCC-end         [1 2]
+            board           [:open-circuitry/board {:width 10 :height 20}
+                             [:juncture {:at GND-coordinates, :trace "GND"}]
+                             [:juncture {:at VCC-start, :trace "VCC"}]
+                             [:juncture {:at VCC-end, :trace "VCC"}]]]
+        (is (isolated? GND-coordinates VCC-start board))
+        (is (isolated? GND-coordinates VCC-end board))
+        (is (not (isolated? VCC-start VCC-end board)))))))
