be54da82c5da4963c00fe4a070e7a489a8773d41
Alex Harms <onealexharms@gmail.com>
2020-11-04 15:35:54 -0500

    Zero-length edges are gone.

--- src/open_circuitry/voronoi.clj
+++ src/open_circuitry/voronoi.clj
@@ -2,6 +2,8 @@
   (:import
    (org.kynosarges.tektosyne.geometry LineD PointD RectD Voronoi)))
 
+(def ^:private epsilon 0.0001)
+
 (defn- ->point [^PointD p]
   [(.x p) (.y p)])
 
@@ -15,27 +17,32 @@
         end          (->point (.end clipped-line))]
     [start end]))
 
+(defn- zero-length? [{[x1 y1] :start, [x2 y2] :end}]
+  (let [x-delta (- x1 x2)
+        y-delta (- y1 y2)
+        length  (Math/sqrt (+ (* x-delta x-delta) (* y-delta y-delta)))]
+    (< length epsilon)))
 
 (defn- edges [tektosyne-voronoi tektosyne-bounds] 
-  (let [generator-points   (vec (map ->point (.generatorSites tektosyne-voronoi)))]
-    (vec (for [tektosyne-edge (.voronoiEdges tektosyne-voronoi)]
-           (let [tektosyne-vertices (.voronoiVertices tektosyne-voronoi)
-                 index-of-vertex1   (.vertex1 tektosyne-edge)
-                 index-of-vertex2   (.vertex2 tektosyne-edge)
-                 [start end]        (bounded-edge 
-                                      tektosyne-bounds
-                                      (get tektosyne-vertices index-of-vertex1)
-                                      (get tektosyne-vertices index-of-vertex2))
-                 index-of-site1     (.site1 tektosyne-edge)
-                 index-of-site2     (.site2 tektosyne-edge)]
-             {:start            start
-              :end              end
-              :generator-points [(get generator-points index-of-site1)
-                                 (get generator-points index-of-site2)]})))))
+  (let [generator-points   (vec (map ->point (.generatorSites tektosyne-voronoi)))
+        edges              (for [tektosyne-edge (.voronoiEdges tektosyne-voronoi)]
+                             (let [tektosyne-vertices (.voronoiVertices tektosyne-voronoi)
+                                   index-of-vertex1   (.vertex1 tektosyne-edge)
+                                   index-of-vertex2   (.vertex2 tektosyne-edge)
+                                   [start end]        (bounded-edge 
+                                                        tektosyne-bounds
+                                                        (get tektosyne-vertices index-of-vertex1)
+                                                        (get tektosyne-vertices index-of-vertex2))
+                                   index-of-site1     (.site1 tektosyne-edge)
+                                   index-of-site2     (.site2 tektosyne-edge)]
+                               {:start            start
+                                :end              end
+                                :generator-points [(get generator-points index-of-site1)
+                                                   (get generator-points index-of-site2)]}))] 
+    (vec (remove zero-length? edges))))
 
 (defn voronoi [points [[left top] [width height]]]
   (let [pointDs             (into-array PointD (map ->PointD points))
         tektosyne-bounds    (RectD. (PointD. left top) (PointD. (+ left width) (+ top height)))
-        tektosyne-voronoi   (Voronoi/findAll pointDs tektosyne-bounds)
-        edges               (edges tektosyne-voronoi tektosyne-bounds)]
-    {:edges edges}))
+        tektosyne-voronoi   (Voronoi/findAll pointDs tektosyne-bounds)]
+    {:edges (edges tektosyne-voronoi tektosyne-bounds)}))
--- test/open_circuitry/isolation_test.clj
+++ test/open_circuitry/isolation_test.clj
@@ -8,6 +8,13 @@
     (or (neg? (read-string x1))
         (neg? (read-string x2)))))
 
+(defn- zero-length? [path]
+  (let [{:keys [x1 y1 x2 y2]} (:attrs path)
+        x (- (read-string x1) (read-string x2))
+        y (- (read-string y1) (read-string y2))
+        length (Math/sqrt (+ (* x x) (* y y)))]
+    (< length 0.01)))  
+
 (deftest an-isolation-toolpath
   (testing "for a board with no junctures"
     (let [board [:open-circuitry/board {:width 10 :height 20}]
@@ -22,4 +29,14 @@
       (testing "has isolation paths"
         (is (not (empty? paths))))
       (testing "has no isolation paths with a negative x"
-        (is (empty? (filter has-negative-x? paths)))))))
+        (is (empty? (filter has-negative-x? paths))))))
+  (testing "for a board with four isolated junctures in a perfrect square"
+    (let [board [:open-circuitry/board {:width 10 :height 20}
+                 [:juncture {:at [3 3]}]
+                 [:juncture {:at [3 5]}]
+                 [:juncture {:at [5 3]}]
+                 [:juncture {:at [5 5]}]]
+          paths (elements-by-selector [:#isolation-toolpath :> :line] board)]
+      (testing "has no zero-length edges"
+        (is (empty? (filter zero-length? paths)))))))
+  
